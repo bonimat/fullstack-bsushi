9a609f96c4e3e5be4271e4d6779c8bac79f37ae6
Matteo Boni <bonimat@hotmail.com>
2025-10-26 10:17:55 +0100

    App with functional components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navabar from './components/navabar';
+import Card from './components/card'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Navabar />
+    <div className="container ">
+      <h1>Cosa desideri ordinare?</h1>
+      <hr />
+      <div className='row'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>  
+  </>
   );
 }
 
---
+++ src/components/navabar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return(
+    <nav className="navbar bg-primary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Fullstack BSushi</span>
+      </div>
+    </nav>);
+    }   
+
+export default Navbar;
